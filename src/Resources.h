8ed7ee89679fa47ca90228feac18c842697e596b
Drillgon200 <drillgon200@gmail.com>
2026-04-18 14:18:16 -0700

    new tiles

--- src/Resources.h
+++ src/Resources.h
@@ -51,6 +51,10 @@ struct {
 	Sprite assemblerLarge;
 	Sprite hive;
 	Sprite hiveLarge;
+	Sprite splitter;
+	Sprite merger;
+	Sprite beeFly;
+	Sprite beeMine;
 	struct {
 		Sprite right;
 		Sprite left;
@@ -83,10 +87,14 @@ void load() {
 	tile.assemblerLarge = Sprite{ &tileset, 0, 96, 32, 32, 1 };
 	tile.hive = Sprite{ &tileset, 16, 32, 16, 16, 1 };
 	tile.hiveLarge = Sprite{ &tileset, 0, 64, 16, 16, 1 };
+	tile.splitter = Sprite{ &tileset, 32, 48, 16, 16, 1 };
+	tile.merger = Sprite{ &tileset, 32, 64, 16, 16, 1 };
+	tile.beeFly = Sprite{ &tileset, 0, 160, 16, 16, 4 };
+	tile.beeMine = Sprite{ &tileset, 0, 144, 16, 16, 4 };
 	tile.belt.right = Sprite{ &tileset, 64, 0, 16, 16, 3 };
-	tile.belt.left = Sprite{ &tileset, 64, 0, 16, 16, 3 };
+	tile.belt.left = Sprite{ &tileset, 64, 160, 16, 16, 3 };
 	tile.belt.up = Sprite{ &tileset, 64, 32, 16, 16, 3 };
-	tile.belt.down = Sprite{ &tileset, 64, 0, 16, 16, 3 };
+	tile.belt.down = Sprite{ &tileset, 64, 176, 16, 16, 3 };
 	tile.belt.bottomToRight = Sprite{ &tileset, 64, 16, 16, 16, 3 };
 	tile.belt.bottomToLeft = Sprite{ &tileset, 64, 64, 16, 16, 3 };
 	tile.belt.leftToUp = Sprite{ &tileset, 64, 80, 16, 16, 3 };
