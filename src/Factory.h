c6f008f87a9d44ae76e763193129a7a98357447d
Drillgon200 <drillgon200@gmail.com>
2026-04-18 18:25:43 -0700

    Machine def

--- src/Factory.h
+++ src/Factory.h
@@ -15,12 +15,41 @@ struct ItemStack {
 	U32 count;
 };
 
+enum MachineType {
+	MACHINE_NONE,
+	MACHINE_BELT,
+	MACHINE_Count
+};
+
 struct Machine {
+	MachineType type;
 	ItemStack inventory;
 	U32 inventoryStackSizeLimit;
 	Machine* output;
 };
 
+struct MachineDef {
+	V2U size;
+};
+
+MachineDef rotate(MachineDef& src, Direction2 direction) {
+	MachineDef result = src;
+}
+
+MachineDef get_belt(Direction2 src, Direction2 dst) {
+	DEBUG_ASSERT(src != dst, "Belt cannot do a U turn"a);
+	MachineDef result{};
+	switch (src) {
+	case DIRECTION2_LEFT: {
+	} break;
+	case DIRECTION2_RIGHT: {
+	} break;
+	case DIRECTION2_FRONT: {
+	} break;
+	case DIRECTION2_BACK: {
+	} break;
+	return result;
+}
 
 ArenaArrayList<Machine*> machineTiles;
 U32 tickCount;
